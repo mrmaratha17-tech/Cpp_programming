3ef04cb4e311738fdcb879bb390fd3bf4cc82caf
mrmaratha17-tech <mrmaratha17@gmail.com>
2026-06-11 13:51:26 +0530

    Add Square and Cube calculation program

---
+++ Basic.codes/Square_cube.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Square = " << num * num << endl;
+    cout << "Cube = " << num * num * num;
+
+    return 0;
+}
