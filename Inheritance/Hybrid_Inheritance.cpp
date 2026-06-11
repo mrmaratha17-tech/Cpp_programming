a3b4ba0d27ad7762cf4cefaee19c759eae26da37
mrmaratha17-tech <mrmaratha17@gmail.com>
2026-06-11 18:39:19 +0530

    hybrid inheritance in C++

---
+++ Inheritance/Hybrid_Inheritance.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+    void showA()
+    {
+        cout << "Class A\n";
+    }
+};
+
+class B : public A
+{
+};
+
+class C : public A
+{
+};
+
+class D : public B, public C
+{
+};
+
+int main()
+{
+    D obj;
+
+    obj.B::showA();
+    obj.C::showA();
+
+    return 0;
+}
