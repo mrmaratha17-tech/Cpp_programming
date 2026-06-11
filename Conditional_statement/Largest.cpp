c3debc3c01951624222eb19d9b31bcd098a21def
mrmaratha17-tech <mrmaratha17@gmail.com>
2026-06-11 14:01:34 +0530

    find the largest of two numbers

---
+++ Conditional_statement/Largest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if(a > b)
+        cout << a << " is largest";
+    else
+        cout << b << " is largest";
+
+    return 0;
+}
