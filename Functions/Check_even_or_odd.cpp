d722677b89fe87594825aedc4ad313b245c888ed
mrmaratha17-tech <mrmaratha17@gmail.com>
2026-06-11 17:47:40 +0530

    check if a number is even or odd

---
+++ Functions/Check_even_or_odd.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void checkEvenOdd(int n)
+{
+    if(n % 2 == 0)
+        cout << "Even";
+    else
+        cout << "Odd";
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    checkEvenOdd(n);
+
+    return 0;
+}
